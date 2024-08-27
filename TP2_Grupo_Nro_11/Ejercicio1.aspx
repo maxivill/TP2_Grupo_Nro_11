05c9bfbd8faff1b294044cbde7e5323081221dc2
maxivill <123villmax232@gmail.com>
2024-08-27 01:48:53 -0300

    Agregar archivos de proyecto.

---
+++ TP2_Grupo_Nro_11/Ejercicio1.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Ejercicio1.aspx.cs" Inherits="TP2_Grupo_Nro_11.Ejercicio1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Ingrese nombre del producto:&nbsp;&nbsp;
+            <asp:TextBox ID="tbProducto1" runat="server"></asp:TextBox>
+            &nbsp;&nbsp;&nbsp; Cantidad:&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="tbCantidad1" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Ingrese nombre del producto:&nbsp;&nbsp;
+            <asp:TextBox ID="tbProducto2" runat="server"></asp:TextBox>
+            &nbsp;&nbsp;&nbsp; Cantidad:&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="tbCantidad2" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <br />
+            <asp:Button ID="btnTabla" runat="server" OnClick="btnTabla_Click" Text="Generar Tabla" />
+            <br />
+            <br />
+            <asp:Label ID="lbTabla" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ TP2_Grupo_Nro_11/Ejercicio1.aspx.designer.cs
@@ -0,0 +1,80 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace TP2_Grupo_Nro_11
+{
+
+
+    public partial class Ejercicio1
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control tbProducto1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbProducto1;
+
+        /// <summary>
+        /// Control tbCantidad1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbCantidad1;
+
+        /// <summary>
+        /// Control tbProducto2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbProducto2;
+
+        /// <summary>
+        /// Control tbCantidad2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbCantidad2;
+
+        /// <summary>
+        /// Control btnTabla.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnTabla;
+
+        /// <summary>
+        /// Control lbTabla.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbTabla;
+    }
+}
